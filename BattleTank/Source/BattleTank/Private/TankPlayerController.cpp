aa4972d78e12d53366c043e9b909788a5b131563
TrojanHorseStudio <66873126+TrojanHorseStudio@users.noreply.github.com>
2021-08-26 03:06:14 +0300

    Implemented Tick() function

--- BattleTank/Source/BattleTank/Private/TankPlayerController.cpp
+++ BattleTank/Source/BattleTank/Private/TankPlayerController.cpp
@@ -3,7 +3,37 @@
 
 #include "TankPlayerController.h"
 
+void ATankPlayerController::BeginPlay()
+{
+    Super::BeginPlay();
+
+    auto ControlledTank = GetControlledTank();
+    if (!ControlledTank)
+    {
+        UE_LOG(LogTemp, Warning, TEXT("Player Controller not possessing a tank"));
+    }
+    else
+    {
+        UE_LOG(LogTemp, Warning, TEXT("Player Controller possessing: %s"), *ControlledTank->GetName());
+    }
+}
+
+void ATankPlayerController::Tick(float DeltaTime)
+{
+    Super::Tick(DeltaTime);
+    AimTowardsCrosshair();
+}
+
 ATank* ATankPlayerController::GetControlledTank() const
 {
     return Cast<ATank>(GetPawn());
+}
+
+void ATankPlayerController::AimTowardsCrosshair()
+{
+    if (!GetControlledTank()) {return;}
+
+    //get world location through crosshair (linetrace)
+    //if it hits landscape then it
+        //tell control tank to aim this point
 }
